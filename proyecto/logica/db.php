1afb82b19c2180877f91d4aa36df78fdd13d319f
tenochrt19-icy <tenochrt19@gmail.com>
2025-11-26 20:16:02 -0600

    Update db.php
    Este es el cambio de credenciales para el hosting

--- proyecto/logica/db.php
+++ proyecto/logica/db.php
@@ -6,15 +6,16 @@ $pass = 'Sword7Rival23@';
 $dbname = 'crud_app';
 // servidor 
 // $host = 'sql113.infinityfree.com';
-//  $user = 'if0_40185860';
-//  $pass = 'EcneF1x9zu';
-//  $dbname = 'if0_40185860_crud_app' ;
+//  $user = 'if0_40185854';
+//  $pass = 'MwvVIhsLbz';
+// $dbname = 'if0_40185854_ico'
 
 $conn = new mysqli($host, $user, $pass, $dbname);
 
 if ($conn->connect_error) {
     die('Error en la conexion, base de datos' . $conn->connect_error);
 }
+
 
 
 ?>
